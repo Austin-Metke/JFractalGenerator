2be0de32abae24abd2680702046705f29723cd5f
Cincere1105 <76901416+Cincere1105@users.noreply.github.com>
2021-08-02 13:24:03 -0700

    Update Main.java
    Added comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,19 +7,19 @@ public class Main {
     public static double WIDTH = 1920;
     public static double HEIGHT = 1080;
 
-
-
-
+    /**
+    *  When the program runs, this builds the fractal image itself and the window to display it
+    */
     public static void main(String[] args) {
 
-        new MainFrame();
-        new SettingsFrame();
+        new MainFrame();  //Using settings in MainFrame()
+        new SettingsFrame();  //Using settings in SettingsFrame()
 
 
-        int[][] iterationsArr = new int[(int)WIDTH][(int)HEIGHT];
+        int[][] iterationsArr = new int[(int)WIDTH][(int)HEIGHT];  //2-D array used for each pixel in the fractal's dimensions; 1920 rows and 1080 columns
 
         Fractal fractal = new Fractal();
-
+        
         fractal.GenerateFractal(fractal.RESTART, fractal.REEND, fractal.IMSTART, fractal.IMEND, WIDTH, HEIGHT, 0, 0, iterationsArr, Fractal.GENERATE_JULIA, fractal.ITERATIONS);
         fractal.Fractal();
 
