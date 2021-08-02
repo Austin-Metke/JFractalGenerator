c5833c5e6bcc50b8de90480f362ea180cc3a7d23
Cincere1105 <76901416+Cincere1105@users.noreply.github.com>
2021-08-02 13:15:06 -0700

    Update MainFrame.java
    Added comments; will soon test to see if the window will open without setBounds (since there's already one in MainPanel() )

--- src/com/company/MainFrame.java
+++ src/com/company/MainFrame.java
@@ -3,13 +3,16 @@ package com.company;
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
-
+    
+    /**
+     * Creates a window to display the fractal
+     */
     MainFrame() {
 
         setTitle("Fractal Generator");
-        setContentPane(new MainPanel());
+        setContentPane(new MainPanel()); //Creates a window with the set settings in MainPanel() in the MainPanel class
         setBounds(0, 0, 1920, 1080);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Exists the program upon window closure
         setVisible(true);
 
     }
