345423b9da22f5eff08f0dc33667720cd8b2d9a1
Cincere1105 <76901416+Cincere1105@users.noreply.github.com>
2021-08-02 13:04:05 -0700

    Update MainPanel.java
    Added comment for MainPanel()

--- src/com/company/MainPanel.java
+++ src/com/company/MainPanel.java
@@ -6,17 +6,22 @@ import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 
 public class MainPanel extends JPanel {
-
+    
     static BufferedImage buffimg = new BufferedImage((int)Main.WIDTH, (int)Main.HEIGHT, 1);
-
+    
+    /**
+     * MainPanel() is the constructor for the main panel;
+     */
     MainPanel() {
 
-        setBounds(0, 0, 1920, 1080);
+        setBounds(0, 0, 1920, 1080); //center of screen, 1920x1080 dimensions
         setVisible(true);
-        setLayout(null);
+        setLayout(null);  //No desired layout, hence null
 
     }
-
+    /**
+     * paintComponent is the method that draws the fractal
+     */
     public void paintComponent(Graphics g) {
         g.drawImage(buffimg, 0, 0, (ImageObserver)null);
         this.repaint();
